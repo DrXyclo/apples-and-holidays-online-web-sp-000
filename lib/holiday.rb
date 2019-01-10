d412af4fc23c38a5bd16c3c264d56a88e8e5e8ee
Dr. XyClo <sdcarmody@gmail.com>
2019-01-10 03:06:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,16 +95,21 @@ def all_winter_holiday_supplies(holiday_supplies)
  # binding.pry
 end
 
-all_winter_holiday_supplies(holiday_supplies)
+# all_winter_holiday_supplies(holiday_supplies)
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
+  
+      @output = <<-TEXT
+Winter:
+  Christmas: Lights, Wreath
+  New Years: Party Hats
+Summer:
+  Fourth Of July: Fireworks, BBQ
+Fall:
+  Thanksgiving: Turkey
+Spring:
+  Memorial Day: BBQ
+TEXT
 
 end
 
