6da330dcbf58d5193e338d0e12a0e0257aaea975
Dr. XyClo <sdcarmody@gmail.com>
2019-01-10 02:36:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,10 +87,10 @@ holiday_supplies = {
 def all_winter_holiday_supplies(holiday_supplies)
   # return an array of all of the supplies that are used in the winter season
   winter_supply_array = holiday_supplies.flat_map do |season, holiday_name|
-                          season == :winter
-                            holiday_name.values.flatten 
+                          if season == :winter ? holiday_name.values.flatten : ""
                            
                         end
+                      end 
   
 
   #winter_supply_array.delete_if {|i| i == nil }
